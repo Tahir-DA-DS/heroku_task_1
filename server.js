d0ff250c05c4dfa4038da9d23ee7387d0a56d462
Tahir-DA-DS <tahir.adeleye@gmail.com>
2022-10-27 14:23:55 +0100

    get request intro

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require("http");
+
+const port = 3000
+
+http.get('http://localhost:3000/', (res) => {
+
+    let myData=''
+    res.on('data', (chunk)=>{myData+=chunk})
+    res.on('end', ()=>{
+      try {const parsedData = JSON.parse(myData)
+        console.log(parsedData)
+      }catch(e){
+        console.error(e.message)
+      }
+    })
+})
+
+
+
+const server= http.createServer((req, res)=>{
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    const data ={slakUsername:'Tahir. A',
+    Backend:true,
+    Age:26,
+    Bio:'I am Tahir popularly refered to as Ola, I am on the Backend Track in HNGi9 participating from a Abuja and I am also a graduate of Statistics'
+  }
+    res.end(JSON.stringify(data));
+    
+
+})
+
+server.listen(process.env.PORT ||port)
